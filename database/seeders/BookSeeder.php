28d0b263e803b24cb0cb66c78b519ad3b067c838
Elisa Goloviatinski <elisa.goloviatinski@he-arc.ch>
2024-09-19 09:54:12 +0200

    Finished TODO-3

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Book::truncate();
+
+        $books = [
+            ['title' => 'Hunter x Hunter n°1', 'pages' => 110, 'quantity' => 1],
+            ['title' => 'Hunter x Hunter n°2', 'pages' => 87, 'quantity' => 10],
+            ['title' => 'Harry and the Poter', 'pages' => 123, 'quantity' => 11],
+            ['title' => 'Jojolands', 'pages' => 300, 'quantity' => 0]
+        ];
+
+        foreach ($books as $book){
+            \App\Models\Book::create(array(
+                'title' => $book["title"],
+                'pages' => $book["pages"],
+                'quantity' => $book["quantity"]
+            ));
+        }
+    }
+}
