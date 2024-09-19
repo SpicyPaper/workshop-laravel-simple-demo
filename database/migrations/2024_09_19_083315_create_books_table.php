d0cef36b88045613dff86ba49b39719fa8b04523
Marilyn TEUSCHER <marilyn.teuscher@he-arc.ch>
2024-09-19 10:41:30 +0200

    added fields on the table

--- database/migrations/2024_09_19_083315_create_books_table.php
+++ database/migrations/2024_09_19_083315_create_books_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+
+            $table->string('title');
+            $table->integer('pages')->unsigned();
+            $table->integer('quantity')->unsigned();
             $table->timestamps();
         });
     }
