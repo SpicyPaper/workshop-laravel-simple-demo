e8c2211bc91ee42fd9483550b9cc083fc491ef63
Bozier Luan <luan.bozier@he-arc.ch>
2024-09-19 10:35:10 +0200

    todo-3

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('books')->insert(
+            [
+                'title' => "Mon livre",
+                'pages' => 10,
+                'quantity' => 20,
+            ],
+            [
+                'title' => "Livre de tom",
+                'pages' => 1,
+                'quantity' => 300,
+            ],
+            [
+                'title' => "Livre de seb",
+                'pages' => 1000,
+                'quantity' => 30,
+            ]
+        );
+    }
+}
