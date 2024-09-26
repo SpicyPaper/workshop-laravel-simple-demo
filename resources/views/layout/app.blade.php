50f37fe7017b3e82251e6a8c3c9459708f722314
Elisa Goloviatinski <elisa.goloviatinski@he-arc.ch>
2024-09-26 11:09:42 +0200

    Finished TODO-9

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,21 +8,22 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/">Home</a>
+            <a class="navbar-brand bi bi-house-door" href="/">Home</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                      <a class="nav-link" href="{{ route('books.index') }}">Books</a>
+                      <a class="nav-link bi bi-bookshelf" href="{{ route('books.index') }}">Books</a>
                   </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="{{ route('books.order') }}">Order</a>
+                      <a class="nav-link bi bi-cart" href="{{ route('books.order') }}">Order</a>
                   </li>
                 </ul>
             </div>
