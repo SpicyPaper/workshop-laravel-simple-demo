cba9b6dd279ea3d3456cc9dc93475f3530f77242
Vivone Tom <tom.vivone@he-arc.ch>
2024-09-26 11:37:19 +0200

    todo 8.5

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Auth;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     use HasFactory;
+
+    public function Author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
